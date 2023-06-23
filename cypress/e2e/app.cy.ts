5401ffff26ceec5b8fc3911567a837ce7700d133
Frank van Wijk <info@frankvanwijk.nl>
2023-06-23 14:45:25 +0200

    Add Percy support

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,6 +1,12 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Home page", () => {
+  it("renders title", () => {
     cy.visit("/");
     cy.get("h1").contains("Vite + React");
+
+    cy.percySnapshot("Counter");
+
+    cy.get("button").click();
+
+    cy.percySnapshot("Counter incremented");
   });
 });
